37f900a0808c7aea3aa318e9eee090b419587969
Jan Beran <janberan@outlook.com>
2014-10-30 15:12:31 -0500

    git config test

---
+++ testing/helloworld_git_test_.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   helloworld_git_test_.c
+ * Author: jbadmin
+ *
+ * Created on October 30, 2014, 2:56 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello world\n");
+    return (EXIT_SUCCESS);
+}
+
