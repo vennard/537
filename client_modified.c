2bc43146e401980b06061cda2a17e7c24c355ae3
John Vennard <vennard@wisc.edu>
2014-11-18 18:43:20 -0600

    moving things around for easier testing - tested ping_test

---
+++ client_modified.c
@@ -0,0 +1,290 @@
+/* Client code 
+ * Sends a request to stream a file (movie) from server, waits for acknowledgment and
+ * keeps receiving data packets until a FIN packet is received.
+ * 
+ * Creation: Jan Beran
+ *
+ * Edited 11/12: JV - Commenting and adding splicing calculations
+ * 	adding splice ratio calculations
+ * 	adding support for communication to all 4 servers
+ */
+
+#include "common.h"
+#include "common.c"
+
+
+// buffers for in/out packets
+static unsigned char pktIn[PKTLEN_DATA] = {0};
+static unsigned char pktOut[PKTLEN_MSG] = {0};
+static pkthdr_common* hdrIn = (pkthdr_common*) pktIn;
+static unsigned char* payloadIn = pktIn + HDRLEN;
+
+// data rate storage
+static int srcpkts[4] = {0};
+static float srcRatio[4] = {0};
+static int sendRatio[4] = {0};
+static struct timeval tvStart, tvRecv, tvCheck, tvSplice;
+static char *saddr[4]; //server ip addresses
+bool started = false;
+
+bool spliceRatio(int rxLen, unsigned char *pkt) {
+    int checkTime, i;
+    //record where packet came from
+    int src = checkRxSrc(rxLen, pktIn, ID_CLIENT);
+    if ((src < 1)||(src > 4)) return false;
+    srcpkts[src - 1]++;
+
+    //timer trigger for splice ratio calculations
+    gettimeofday(&tvCheck, NULL);
+    if (!started) {
+        checkTime = timeDiff(&tvStart, &tvCheck);
+        started = true;
+    } else {
+        checkTime = timeDiff(&tvSplice, &tvCheck); 
+    }
+    gettimeofday(&tvSplice, NULL);
+    if (checkTime > SPLICE_DELAY) {
+        int total = srcpkts[0] + srcpkts[1] + srcpkts[2] + srcpkts[3];
+        for (i = 0;i < 4;i++) srcRatio[i] = srcpkts[i] / total;
+        float check = 0;
+        for (i = 0;i < 4;i++) check+=srcRatio[i];
+        if (check != 1) {
+            printf("Error with splice ratio check (= %.6f)\n",check);
+            return false;
+        }
+        //multiply ratio vs SPLICE_FRAME to find final ratio
+        for (i = 0;i < 4;i++) sendRatio[i] = (int) srcRatio[i] * SPLICE_FRAME;
+        
+        //TODO check if ratio's exceed threshold to send to servers
+        
+        //TODO send ratio to servers
+        //fillpkt(pkt, ID_CLIENT, 1, TYPE_SPLICE, 0, (unsigned char*) srcRatio[i], sizeof(srcRatio[i]))
+    
+        //TODO check for acknowledges from servers
+        
+    }
+
+    return true;
+}
+
+bool plotGraph(void) {
+    char cmd[strlen(GRAPH_DATA_FILE) + strlen(GNUPLOT_SCRIPT) + strlen(GRAPH_OUTPUT_FILE) + 10];
+
+    // set gnuplot script permissions
+    snprintf(cmd, sizeof (cmd), "chmod +x %s", GNUPLOT_SCRIPT);
+    if (system(cmd) != 0) {
+        return false;
+    }
+
+    // execute the script
+    snprintf(cmd, sizeof (cmd), "./%s %s %s", GNUPLOT_SCRIPT, GRAPH_DATA_FILE, GRAPH_OUTPUT_FILE);
+    if (system(cmd) != 0) {
+        return false;
+    }
+
+    return true;
+}
+
+bool reqFile(int soc, char* filename) {
+    struct sockaddr_in sender;
+    struct sockaddr_in server[4];
+    unsigned char pkt[4][PKTLEN_MSG];
+    unsigned int senderSize = sizeof (sender);
+    unsigned int errCount = 0;
+    unsigned int serverReq[4] = {0, 0, 0, 0};
+    unsigned int serverAck[4] = {0, 0, 0, 0};
+    bool done = false;
+
+    //create targets and fill request data for all servers
+    int i;
+    for (i = 0;i < 4;i++) {
+        if (initHostStruct(&server[i], saddr[i], UDP_PORT) == false) return false;
+        if (fillpkt(pkt[i], ID_CLIENT, i+1, TYPE_REQ, 0, (unsigned char*) filename, strlen(filename)) == false) return false; 
+    }
+
+    //TODO start time from just before sending requests
+    // send the request and receive a reply
+    gettimeofday(&tvStart, NULL); //start time from acknowledge of start request
+    while (errCount++ < MAX_ERR_COUNT) {
+        int i;
+        for (i = 0;i < 4;i++) {
+            //send request if not received ack
+            if (serverAck[i] != 1) sendto(soc, pkt[i], PKTLEN_MSG, 0, (struct sockaddr*) &server[i], sizeof(server[i]));
+
+            //read acks from servers
+            memset(pktIn, 0, PKTLEN_DATA);
+            int rxRes = recvfrom(soc, pktIn, PKTLEN_MSG, 0, (struct sockaddr*) &sender, &senderSize);
+            rxRes = checkRxStatus(rxRes, pktIn, ID_CLIENT);
+            if (rxRes == RX_TERMINATED) return false;
+            if (rxRes != RX_OK) continue; 
+            if ((hdrIn->src > 4)||(hdrIn->src < 1)) {
+                printf("Error: invalid server source\n");
+                return false;
+            }
+            if (hdrIn->type == TYPE_REQACK) {
+                serverAck[hdrIn->src-1] = 1; 
+                printf("Got ack from server %i!\n",hdrIn->src);
+            } else if (hdrIn->type == TYPE_REQNAK) {
+                printf("Error: Server %i refused to stream the requested file\n",hdrIn->src);
+                return false;
+            } else { //got invalid type
+                printf("Warning: received unexpected packet type, ignoring it\n");
+            }
+        }
+        //check for acks from each server
+        done = true;
+        for (i = 0;i < 4;i++) {
+            if (serverAck[i] == 0) done = false;
+        }
+        if (done) {
+            printf("Received acks from all servers!\n");
+            return true;
+        }
+    }
+        
+/*
+sendto(soc, pktOut, PKTLEN_MSG, 0, (struct sockaddr*) &server, sizeof (server)); dprintPkt(pktOut, PKTLEN_MSG, true);
+        memset(pktIn, 0, PKTLEN_DATA);
+        int rxRes = recvfrom(soc, pktIn, PKTLEN_MSG, 0, (struct sockaddr*) &sender, &senderSize);
+        rxRes = checkRxStatus(rxRes, pktIn, ID_CLIENT);
+
+        if (rxRes == RX_TERMINATED) return false;
+        if (rxRes != RX_OK) continue;
+        if (hdrIn->type == TYPE_REQACK){
+    		  gettimeofday(&tvStart, NULL); //start time from acknowledge of start request
+			  return true;
+		  }
+        if (hdrIn->type == TYPE_REQNAK) {
+            printf("Error: Server refused to stream the requested file\n");
+            return false;
+        }
+
+        printf("Warning: Received an unexpected packet type, ignoring it\n");
+    }
+    */
+    printf("Error: Maximum number of request attempts reached\n");
+    return false;
+}
+
+bool receiveMovie(int soc, char** filename) {
+    struct sockaddr_in sender;
+    unsigned int senderSize = sizeof (sender);
+    unsigned int errCount = 0;
+    if (strcmp(*filename, TEST_FILE) == 0) *filename = "random";
+    char streamedFilename[strlen(*filename) + 10];
+    snprintf(streamedFilename, strlen(*filename) + 10, "client_%s", *filename);
+    FILE* graphDataFile = fopen(GRAPH_DATA_FILE, "w");
+    FILE* streamedFile = fopen(streamedFilename, "wb");
+    if ((graphDataFile == NULL) || (streamedFile == NULL)) {
+        printf("Error: Local data file could not be opened, program stopped\n");
+        return false;
+    }
+
+    while (errCount < MAX_ERR_COUNT) {
+        memset(pktIn, 0, PKTLEN_DATA);
+        int rxLen = recvfrom(soc, pktIn, PKTLEN_DATA, 0, (struct sockaddr*) &sender, &senderSize);
+        gettimeofday(&tvRecv, NULL); // get a timestamp
+
+        int rxRes = checkRxStatus(rxLen, pktIn, ID_CLIENT);
+        if (rxRes == RX_TERMINATED) {
+            fclose(graphDataFile);
+            fclose(streamedFile);
+            return false;
+        } else if (rxRes != RX_OK) {
+            errCount++;
+            continue;
+        }
+
+        if (hdrIn->type == TYPE_FIN) {
+            fclose(graphDataFile);
+            fclose(streamedFile);
+            return true;
+        } else if (hdrIn->type != TYPE_DATA) {
+            printf("Warning: Received an unexpected packet type, ignoring it\n");
+            errCount++;
+            continue;
+        }
+
+        if (spliceRatio(rxLen, pktIn) == false) return false; //access to splice ratio check and calculation
+
+		        unsigned int diff = timeDiff(&tvStart, &tvRecv);
+        if ((diff == UINT_MAX) || (fprintf(graphDataFile, "%u %u\n", diff, hdrIn->seq) < 0)) {
+            printf("Warning: Graph data file write error\n");
+        }
+        if (fwrite(payloadIn, 1, rxLen - HDRLEN, streamedFile) != (rxLen - HDRLEN)) {
+            printf("Warning: Streamed local file write error\n");
+        }
+    }
+
+    printf("Error: Received maximum number of subsequent bad packets\n");
+    fclose(graphDataFile);
+    fclose(streamedFile);
+    return false;
+}
+
+char* checkArgs(int argc, char *argv[]) {
+  char *filename;
+  if ((argc != 6) && (argc != 5)) {
+	 printf("Usage: %s <server 1 ip> <server 2 ip> <server 3 ip> <server 4 ip> [<requested file>]\n",argv[0]);
+	 exit(1);
+  } else if (argc == 6) {
+	 filename = argv[5];
+	 if (strlen(filename) > MAX_FILENAME_LEN) {
+		printf("Error: Filename too long\n");
+		exit(1);
+	 }
+  } else {
+	 filename = TEST_FILE;
+  }
+  saddr[0] = argv[1];
+  saddr[1] = argv[2];
+  saddr[2] = argv[3];
+  saddr[3] = argv[4];
+  return filename;
+}
+
+int main(int argc, char *argv[]) {
+    char* filename = checkArgs(argc, argv);
+
+	 // initialize UDP socket
+    int soc = udpInit(UDP_PORT + 1, RECV_TIMEOUT);
+    if (soc == -1) {
+        printf("Error: UDP socket could not be initialized, program stopped\n");
+        exit(1);
+    } else {
+        dprintf("UDP socket initialized, SOCID=%d\n", soc);
+    }
+
+	 // start transmission of file
+    printf("Requesting file '%s' from servers with the following addresses\n", filename);
+    int i;
+    for (i = 0;i < 4;i++) printf("server%i: %s\n", i, saddr[i]);
+
+    if (reqFile(soc, filename) == false) { 
+        printf("Error: Request failed, program stopped\n");
+        close(soc);
+        exit(1);
+    } else {
+        printf("Request for '%s' successful, receiving the data\n", filename);
+    }
+	exit(0); //DEBUG STOP TODO
+
+	 // receive movie
+    if (receiveMovie(soc, &filename) == false) {
+        printf("Error: Error during the file streaming, program stopped\n");
+        close(soc);
+        exit(1);
+    } else {
+        printf("File successfully transfered, local copy: client_%s\n", filename);
+    }
+
+    // clean up resources and plot 
+    close(soc);
+    if (plotGraph() == false) {
+        printf("Warning: Graph could not be plotted\n");
+    } else {
+        dprintf("RX packet rate graph: '%s', raw data: '%s'\n", GRAPH_OUTPUT_FILE, GRAPH_DATA_FILE);
+    }
+
+    return 0;
+}
