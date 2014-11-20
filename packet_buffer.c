5ec3fd3ae117f483bb65eb376566dad1882c44fe
Jan Beran <janberan@outlook.com>
2014-11-20 01:46:02 -0600

    First version of the packet_buffer component. Compilable but not tested yet.

---
+++ packet_buffer.c
@@ -0,0 +1,199 @@
+/* Definitions of packet buffer functions
+ * See the header file for detailed description
+ *
+ * Jan Beran
+ */
+
+#include "packet_buffer.h"
+
+/*******************
+ * Local variables
+ *******************/
+
+// packet structure in the buffer
+
+typedef struct pkt_buffer {
+    bool isFree;
+    uint32_t seq; // sequence number
+    unsigned char data[DATALEN];
+} pkt_buffer;
+
+static pkt_buffer buf[BUF_SIZE]; // buffer itself
+static unsigned int headInd = 0; // buffer starts at
+static uint32_t headSeq = 1; // seq at the buffer start (present or expected)
+static uint32_t lastSeq = 0; // last seq in buffer
+static uint32_t lastReqSeq = 0; // last requested lost seq
+static bool initialized = false;
+static FILE* out = NULL;
+
+/*******************
+ * Private functions
+ *******************/
+
+static unsigned int getCount(void) {
+    if (lastSeq == 0) return 0;
+    return (lastSeq - headSeq + 1);
+}
+
+static int checkScope(uint32_t seq) {
+    if (seq < headSeq) return BUF_SEQ_OLD;
+    if (seq >= headSeq + BUF_SIZE) return BUF_SEQ_HIGH;
+
+    // get index in the buffer corresponding to seq
+    int index = (int) ((headInd + (seq - headSeq)) % BUF_SIZE);
+
+    // is packet with seq already in the buffer?
+    if (buf[index].isFree == false) return BUF_SEQ_EXIST;
+
+    // no, it is empty, return the index
+    return index;
+}
+
+/*******************
+ * Public functions
+ *******************/
+
+bool bufInit(char* filename) {
+
+    if (filename != NULL) {
+        // open the output file
+        out = fopen(filename, "wb");
+        if (out == NULL) {
+            printf("Error: Local data file could not be opened, program stopped\n");
+            return false;
+        }
+    }
+
+    // set init values
+    for (int i = 0; i < BUF_SIZE; i++) {
+        buf[i].isFree = true;
+        buf[i].seq = 0;
+    }
+
+    initialized = true;
+    return true;
+}
+
+bool bufFinish(void) {
+    if (!initialized) return false;
+
+    if (out != NULL) {
+        fclose(out);
+    }
+    initialized = false;
+    return true;
+}
+
+bool bufAdd(uint32_t seq, unsigned char* data) {
+    if ((!initialized) || (data == NULL) || (seq == 0)) return false;
+
+    int index = checkScope(seq);
+    switch (index) {
+        case BUF_SEQ_OLD:
+            dprintf("Warning: attempt to insert already flushed packet in the buffer, seq=%u\n", seq);
+            return true;
+        case BUF_SEQ_HIGH:
+            printf("Warning: attempt to insert too high seq in the buffer, packet dropped, seq=%u\n", seq);
+            return false;
+        case BUF_SEQ_EXIST:
+            dprintf("Warning: attempt to insert pkt already present in the buffer, seq=%u\n", seq);
+            return true;
+        default:
+            dprintf("Packet Inserted in the buffer, seq=%u index=%d\n", seq, index);
+            break;
+    }
+
+    // set the packet data
+    buf[index].isFree = false;
+    buf[index].seq = seq;
+    memcpy(buf[index].data, data, DATALEN);
+
+    // update the last seq in the buffer
+    if (seq > lastSeq) {
+        lastSeq = seq;
+    }
+
+    return true;
+}
+
+unsigned int bufGetSubseqCount(void) {
+    if (!initialized) return 0;
+
+    unsigned int count = 0; // packet counter
+    unsigned int ind = headInd; // index in the buffer
+    while (count < BUF_SIZE) {
+        if (buf[ind].isFree == true) break; // stop at the first free cell
+        count++;
+        ind = (ind + 1) % BUF_SIZE;
+    }
+    return count;
+}
+
+bool bufFlushFrame(void) {
+    if (!initialized) return false;
+
+    // check if there is a packet at the beginning of the buffer
+    if (buf[headInd].isFree == true) {
+        printf("Error: Missing frame to be played, cannot continue streaming, expected seq=%u\n", headSeq);
+        return false;
+    }
+
+    // flush the packet
+    if (out != NULL) {
+        if (fwrite(buf[headInd].data, 1, DATALEN, out) != (DATALEN)) {
+            printf("Warning: local data file write error\n");
+        }
+    }
+    dprintf("Packet flushed from the buffer, seq=%u index=%d\n", buf[headInd].seq, headInd);
+
+    // update the buffer head
+    buf[headInd].isFree = true;
+    headInd = (headInd + 1) % BUF_SIZE;
+    headSeq++;
+    if (lastSeq < headSeq) {
+        // buffer is empty
+        lastSeq = 0;
+    }
+
+    return true;
+}
+
+double bufGetOccupancy(void) {
+    if (!initialized) return 0;
+    return (((double) getCount()) / BUF_SIZE);
+}
+
+uint32_t bufGetFirstLost(void) {
+    lastReqSeq = 0; // reset the requested packets memory
+    return bufGetNextLost();
+}
+
+uint32_t bufGetNextLost(void) {
+    if (!initialized) return 0;
+    unsigned int pktCount = getCount();
+    if (pktCount <= BUF_LOST_THRSH) return 0; // not enough packets to have a lost one
+    if (lastReqSeq >= lastSeq - BUF_LOST_THRSH) return 0; // already requested all possible losses
+    if (lastReqSeq < headSeq) lastReqSeq = 0; // last requested seq too old
+
+    // index in the buffer 
+    unsigned int ind;
+    if (lastReqSeq == 0) {
+        // start from the beginning
+        ind = headInd;
+    } else {
+        // start from the last request
+        ind = (headInd + (lastReqSeq - headSeq + 1)) % BUF_SIZE;
+    }
+
+    unsigned int count = 0; // packet counter
+    while (count < (pktCount - BUF_LOST_THRSH)) {
+        if (buf[ind].isFree == true) {
+            // lost packet detected
+            lastReqSeq = headSeq + count; // remember its seq
+            return (headSeq + count); // request it
+        }
+        count++;
+        ind = (ind + 1) % BUF_SIZE;
+    }
+    return 0;
+}
---
+++ packet_buffer.h
@@ -0,0 +1,114 @@
+/* Interface of the packet buffer component
+ * Implemented as a fixed-length ring buffer
+ *
+ * Jan Beran
+ */
+
+#ifndef PACKET_BUFFER_H
+#define	PACKET_BUFFER_H
+
+#include "common.h"
+
+/*******************
+ * Buffer defines
+ *******************/
+
+#define BUF_SIZE 1000 // size (pkts) of the packet buffer (in client)
+#define BUF_LOST_THRSH 100 // missing packets older than seq=(newest seq)-LOST_THRSH are considered as lost 
+
+// possible states of the received packet - internal use only
+#define BUF_SEQ_OLD -3  // seq < beginning of the buffer
+#define BUF_SEQ_HIGH -2 // seq > end of the buffer
+#define BUF_SEQ_EXIST -1// seq already in the buffer
+
+
+/*******************
+ * Public functions
+ *******************/
+
+/* 
+ * bufInit
+ * 
+ * Initialize the buffer, must be called prior any other buffer function
+ * 
+ * filename: name of the output file, NULL if no file used 
+ * 
+ * Return value: true if initialized, fail otherwise
+ */
+bool bufInit(char* filename);
+
+/* 
+ * bufFinish
+ * 
+ * Clean up allocated resources, should be called when the streaming is finished
+ * 
+ * Return value: true if successfully finished, fail otherwise
+ */
+bool bufFinish(void);
+
+/* 
+ * bufAdd
+ * 
+ * Add a new (received) packet in the buffer
+ * 
+ * seq: seq number of the packet
+ * data: pointer to the actual packet data (DATALEN size)
+ * 
+ * Return value:    false if the packet has to be dropped (seq too high to get in the buffer)
+ *                  true if the packet is successfully inserted or if it is an already processed packet
+ */
+bool bufAdd(uint32_t seq, unsigned char* data);
+
+/* 
+ * bufGetSubseqCount
+ * 
+ * Get number of subsequent seq numbers at beginning of the buffer (number of packets until a "hole")
+ * 
+ * Return value: 0 if error, number of subsequent packets otherwise (can be 0 as well)
+ */
+unsigned int bufGetSubseqCount(void);
+
+/* 
+ * bufFlushFrame
+ * 
+ * Flush a frame from the beginning of the buffer. Frame is written to the output file or
+ * discarded if there is no file. Expected to be called every 10ms. 
+ * 
+ * Return value:    true if frame successfully flushed, 
+ *                  false if error or if there is missing packet at the beginning of the buffer
+ */
+bool bufFlushFrame(void);
+
+/* 
+ * bufGetOccupancy
+ * 
+ * Get ratio (percentage) of buffer occupancy. 
+ * Can be used to check if we are running out of the space in the buffer,
+ * 
+ * Return value: floating point value within <0,1>, ratio of occupancy (0 empty, 1 full)
+ */
+double bufGetOccupancy(void);
+
+/* 
+ * bufGetFirstLost
+ * 
+ * Get sequential number of the first (oldest) lost packet. 
+ * Reset the requested packets memory.
+ * 
+ * Return value: 0 if there no lost packet in the buffer, seq of the first lost packet otherwise
+ */
+uint32_t bufGetFirstLost(void);
+
+/* 
+ * bufGetNextLost
+ * 
+ * Get sequential number of the next lost packet according to the the requested packets memory.
+ * Should be called in a cycle until there are no more missing packets. 
+ * Then the next call should be bufGetFirstLost() again.
+ * 
+ * Return value: 0 if there no next lost packet, seq of the next lost packet otherwise
+ */
+uint32_t bufGetNextLost(void);
+
+#endif	/* PACKET_BUFFER_H */
+
