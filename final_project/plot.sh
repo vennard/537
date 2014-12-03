6db0fbaff9379b44b52244c83f0892f76f5d5edb
John Vennard <vennard@wisc.edu>
2014-12-02 22:14:36 -0600

    finished plot.sh file

--- final_project/plot.sh
+++ final_project/plot.sh
@@ -1,44 +1,63 @@
 #!/bin/bash
+#
+# usage: ./plot.sh <data file>
+# 
+# outputs:
+#   out.pdf - graph of data points and slope lines
+#   out.txt - data file containing all delay values with associated R values (10->100)
+#
+# note: may have to give permission using chmod a+x plot.sh
 
-#test graph
 FILE=$1
-RVALUE=$2
+SLOPE_ARRAY=()
+DELAY_ARRAY=()
 
-DELAY=0
-echo Calculating max delay using file: $FILE and slope $SLOPE:
+echo Calculating max delay using file: $FILE 
+echo ECE537 ATeam Project Delay Values > out.txt
+echo John Vennard \& Jan Beran >> out.txt
+echo Input file: $FILE >> out.txt
 
-SLOPE=`echo "$RVALUE / 1000" | bc -l`
-echo Slope = $SLOPE
-
-while read line;do
-    eval vals=($line)
-    X=${vals[0]}
-    Y=${vals[1]}
-    y1=`echo "$Y / $SLOPE" | bc` 
-    echo Y1 is $y1
-    d=`echo "$y1 - $X" | bc`
-    echo d is $d
-    dx=`echo "$d * -1" | bc`
-    echo dx is $dx
-    if [ $dx -gt $DELAY ];then
-        DELAY=$dx
-    fi
-    let k++
-done < $FILE
-
-echo MAX DELAY = $DELAY
+# Loop over each R value increment by 10
+for i in {10..100..10}; do
+    DELAY=0
+    # Calculate slope in frame/ms
+    SLOPE=`echo "$i / 1000" | bc -l`
+    SLOPE_ARRAY+=($SLOPE)
+    # calculate Delay = Y/R - X
+    while read line;do
+        eval vals=($line)
+        X=${vals[0]}
+        Y=${vals[1]}
+        y1=`echo "$Y / $SLOPE" | bc` 
+        d=`echo "$y1 - $X" | bc`
+        dx=`echo "$d * -1" | bc`
+        if [ $dx -gt $DELAY ];then
+            DELAY=$dx
+        fi
+        let k++
+    done < $FILE
+    echo MAX DELAY with R of $i = $DELAY
+    echo R = $i results in max delay = $DELAY ms >> out.txt
+    DELAY_ARRAY+=($DELAY)
+done
 
 gnuplot <<- EOF
-    set yrange [0:10]
+    set yrange [0:*]
     set xlabel "ms"
     set ylabel "frame"
-    set term svg
+    set term pdf 
     set nokey
-    set output "out.svg"
-    F(x) = $SLOPE * (x - $DELAY)
-    plot "$FILE", F(x) 
+    set output "out.pdf"
+    F1(x) = ${SLOPE_ARRAY[0]} * (x - ${DELAY_ARRAY[0]})
+    F2(x) = ${SLOPE_ARRAY[1]} * (x - ${DELAY_ARRAY[1]})
+    F3(x) = ${SLOPE_ARRAY[2]} * (x - ${DELAY_ARRAY[2]})
+    F4(x) = ${SLOPE_ARRAY[3]} * (x - ${DELAY_ARRAY[3]})
+    F5(x) = ${SLOPE_ARRAY[4]} * (x - ${DELAY_ARRAY[4]})
+    F6(x) = ${SLOPE_ARRAY[5]} * (x - ${DELAY_ARRAY[5]})
+    F7(x) = ${SLOPE_ARRAY[6]} * (x - ${DELAY_ARRAY[6]})
+    F8(x) = ${SLOPE_ARRAY[7]} * (x - ${DELAY_ARRAY[7]})
+    F9(x) = ${SLOPE_ARRAY[8]} * (x - ${DELAY_ARRAY[8]})
+    plot "$FILE", F1(x), F2(x), F3(x), F4(x), F5(x), F6(x), F7(x), F8(x), F9(x) 
 EOF
-
-
-
+echo done
 exit 0
