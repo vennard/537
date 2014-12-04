a34e96e7196a3a6ec4b19e5a7b7b05e640f1d374
vennard <vennard@wisc.edu>
2014-12-03 18:26:16 -0600

    updating values

--- final_project/common.h
+++ final_project/common.h
@@ -36,7 +36,7 @@
 #define SPLICE_DELAY 10000 //time between calculating splice ratios
 #define SPLICE_FRAME 100  //controls resolution of splice ratio from servers
 #define SPLICE_THRESH 5  //TODO threshold of change needed to send update
-#define SPLICE_GAP 30 // + last received packet syncs splice changeover at servers
+#define SPLICE_GAP 200 // + last received packet syncs splice changeover at servers
 #define SPLICE_IGNORE_THRESH 20 //threshold for excluding node from splice ack
 //above is 1/value for percentage of splice ratio to fall below to ignore
 
